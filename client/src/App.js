cf7451c897c80e08702c7942db19705737d4e043
Denis McPhillips <denismcphillips@gmail.com>
2020-05-21 01:46:07 -0400

    rendering title, artist, album

--- client/src/App.js
+++ client/src/App.js
@@ -14,11 +14,37 @@ class App extends Component {
         name: "Not Checked",
         image: "",
       },
+      chosenPlaylistTitle: "May 2020",
+      chosenPlaylist: {},
+      chosenPlaylistTracks: [],
     };
     if (params.access_token) {
       spotifyWebApi.setAccessToken(params.access_token);
     }
+    this.getNowPlaying = this.getNowPlaying.bind(this);
   }
+
+  async componentDidMount() {
+    await this.getNowPlaying();
+
+    const playlists = await spotifyWebApi.getUserPlaylists();
+    const chosenPlaylist = await playlists.items.find(
+      (playlist) => playlist.name === this.state.chosenPlaylistTitle
+    );
+    await this.setState({ chosenPlaylist });
+    const tracksResponse = await spotifyWebApi.getPlaylistTracks(
+      this.state.chosenPlaylist.id
+    );
+    await this.setState({ chosenPlaylistTracks: tracksResponse.items });
+  }
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const currentSong = await spotifyWebApi.getMyCurrentPlayingTrack();
+  //   console.log(this.state.nowPlaying.name);
+  //   // if (currentSong.item.name !== this.state.nowPlaying.name) {
+  //   //   console.log("changed");
+  //   // }
+  // }
 
   getHashParams() {
     var hashParams = {};
@@ -42,7 +68,25 @@ class App extends Component {
     });
   }
 
+  async skip() {
+    await spotifyWebApi.skipToNext();
+    // const newSong = await spotifyWebApi.getMyCurrentPlayingTrack();
+    // await this.setState({
+    //   name: newSong.item.name,
+    //   image: newSong.item.album.images[0].url,
+    // });
+  }
+
   render() {
+    if (!this.state.loggedIn) {
+      return (
+        <div>
+          <a href="http://localhost:8888">
+            <button>Login With Spotify</button>
+          </a>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <a href="http://localhost:8888">
@@ -53,10 +97,37 @@ class App extends Component {
           <img src={this.state.nowPlaying.image} style={{ width: 100 }} />
         </div>
         <button onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+        <button onClick={() => spotifyWebApi.seek(0)}>Previous</button>
         <button onClick={() => spotifyWebApi.pause()}>Pause</button>
         <button onClick={() => spotifyWebApi.play()}>Play</button>
-        <button onClick={() => spotifyWebApi.skipToNext()}>Skip</button>
-        {/* <button onClick={() => this.pause()}>Pause</button> */}
+        <button onClick={this.skip}>Skip</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.chosenPlaylistTracks.map((trackObject) => {
+              return (
+                <tr key={trackObject.track.id}>
+                  <td>{trackObject.track.name}</td>
+                  <td>
+                    {trackObject.track.artists.map((artist, idx) =>
+                      idx === trackObject.track.artists.length - 1
+                        ? `${artist.name}`
+                        : `${artist.name}, `
+                    )}
+                  </td>
+                  {/* <td>{trackObject.track.artist}</td> */}
+                  <td>{trackObject.track.album.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
